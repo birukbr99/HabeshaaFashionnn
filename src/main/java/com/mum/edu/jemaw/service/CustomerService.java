3ead0aa64e1f8f0e818fe6ddf768e02a651b7e00
gezasami <samischola@gmail.com>
2015-03-17 18:49:08 -0500

    eee

--- src/main/java/com/mum/edu/jemaw/service/CustomerService.java
+++ src/main/java/com/mum/edu/jemaw/service/CustomerService.java
@@ -1,14 +1,8 @@
 package com.mum.edu.jemaw.service;
-import java.util.List;
 
-import com.mum.edu.jemaw.model.Customer;
+
+
 
 public interface CustomerService {
-
-	List<Customer> getAllCustomeres();
-
-	Customer getCustomer(long id);
-
-	void saveCustomer(Customer customer);
 
 }
